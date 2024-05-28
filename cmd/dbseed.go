da1ad123e4b6270687b133ef8ec969a64f54c8b3
Chris Mague <github@mague.com>
2024-05-28 14:09:49 -0700

    prettify output

--- cmd/dbseed.go
+++ cmd/dbseed.go
@@ -33,10 +33,10 @@ Including users, organizations, and a product catalog.`,
 		}
 		catalog.DbLoad(db)
 		customers.DbLoad(db)
-    fmt.Println("")
-    if debug {
-    fmt.Printf("\nDatabase seeded with %d customers and %d products\n", customerCount, productCount)
-    }
+		fmt.Println("") // Blank line to that it looks nice
+		if debug {
+			fmt.Printf("\nDatabase seeded with %d customers and %d products\n", customerCount, productCount)
+		}
 
 	},
 }
